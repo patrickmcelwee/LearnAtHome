4d38956d611951e65ee9ceff2fa826c291da50b5
Patrick McElwee <pmcelwee@gmail.com>
2012-03-25 10:27:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolPlanner::Application.config.secret_token = 'e8ac3e338ff3a4e7294ace0e98f3060e270f436f3ff5b7b0ba128d4fd9f55db98aa94d0e3b78e2847529aec97d73da70b3fce4fe273b697c39a1e53a17fe5a14'
